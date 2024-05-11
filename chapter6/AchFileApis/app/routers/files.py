a6a188f131bafa2834f6fbe9f635a10f9f1eb7c3
Development at SCTTECH.COM <scttech@users.noreply.github.com>
2024-05-11 08:18:16 -0400

    Add files via upload

---
+++ chapter6/AchFileApis/app/routers/files.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Request, status, File, UploadFile
+import hashlib
+import psycopg
+from app.database import DbUtils
+
+router = APIRouter(prefix="/api/v1/files")
+
+@router.get("")
+async def read_files():
+    return [{"file": "File_1"}, {"file": "File_2"}]
+
+@router.get("/{file_id}")
+async def read_file(file_id: str) -> str:
+    return {"file_id": "1"}
+
+@router.post("", status_code=status.HTTP_201_CREATED)
+async def create_file(file: UploadFile = File(...)):
+    contents = await file.read()
+    md5_hash  = hashlib.md5(contents).hexdigest()
+    with DbUtils.get_db_connection() as conn:
+        conn.execute(
+            f"""
+            INSERT INTO ach_files (file_name, file_hash)
+            VALUES (%s, %s)
+            """, (file.filename, md5_hash)
+        )
+    return None
+
+@router.get("/{file_id}/records")
+async def read_file_records(file_id: str):
+    return [{"record": "Record_1"}, {"record": "Record_2"}]
+
+@router.get("/{file_id}/records/{record_id}")
+async def read_file_record(file_id: str, record_id: str):
+    return {"file_id": file_id, "record_id": record_id}
