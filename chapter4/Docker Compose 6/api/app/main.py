5c2741197786fd7ddf0312249055741782e8841b
Development at SCTTECH.COM <scttech@users.noreply.github.com>
2024-05-11 06:25:55 -0400

    Add files via upload

---
+++ chapter4/Docker Compose 6/api/app/main.py
@@ -0,0 +1,32 @@
+from typing import Union
+
+from fastapi import FastAPI
+import psycopg
+
+DATABASE_URL = "dbname=postgres user=postgres password=secret host=postgres port=5432"
+
+app = FastAPI()
+
+def get_db():
+    conn = psycopg.connect(DATABASE_URL)
+    return conn
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World!"}
+
+
+@app.get("/health/status")
+def health_status():
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id, status FROM health_check;")
+
+    health_checks = [{"id": id, "status": status} for (id, status) in cursor.fetchall()]
+
+    cursor.close()
+    conn.close()
+
+    return health_checks
---
+++ chapter4/Docker Compose 8/api/app/main.py
@@ -0,0 +1,45 @@
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import psycopg
+
+DATABASE_URL = "dbname=postgres user=postgres password=secret host=postgres port=5432"
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+def get_db():
+    conn = psycopg.connect(DATABASE_URL)
+    return conn
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World!"}
+
+
+@app.get("/health/status")
+def health_status():
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id, status FROM health_check;")
+
+    health_checks = [{"id": id, "status": status} for (id, status) in cursor.fetchall()]
+
+    cursor.close()
+    conn.close()
+
+    return health_checks
